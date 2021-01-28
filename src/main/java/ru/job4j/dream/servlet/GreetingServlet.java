c11da2c79e7ee6f4614a947e62eff1e63cc3b64b
Александр <46612532+vovkalexander@users.noreply.github.com>
2021-01-28 23:14:32 +0300

    4.1. JSON формат.

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,5 +1,7 @@
 package ru.job4j.dream.servlet;
 
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +11,16 @@ import java.io.PrintWriter;
 
 public class GreetingServlet  extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/json");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
         resp.setHeader("Access-Control-Allow-Origin", "*");
+        JSONObject json = new JSONObject();
+        json.append("key", "Nice to meet you, " + name);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        writer.println(json);
         writer.flush();
     }
 }
